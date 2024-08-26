ee490d8ee50399851e78f279923365d41adef415
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2024-08-26 17:39:55 -0300

    criacao do projeto

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod cli;
+use std::path::PathBuf;
+
+pub enum Action {
+    Add { task: String },
+    Done { position: usize },
+    List,
+}
+
+pub struct CommandLineArgs {
+    pub action: Action,
+    pub journal_file: Option<PathBuf>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
