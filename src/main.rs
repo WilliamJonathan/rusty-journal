149bb63d0722cdcfc6d87976d50e4e8b5a56514a
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2024-09-06 17:27:51 -0300

    funcao add task ( aula 05 )

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cli;
 use structopt::StructOpt;
 
+
 fn main() {
     println!("{:#?}", cli::CommandLineArgs::from_args());
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,9 @@
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::fs::{File, OpenOptions};
+use std::path::PathBuf;
+use std::io::{Result, Seek, SeekFrom};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -14,4 +17,29 @@ impl Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    //Abra o arquivo.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    // Consome o conteúdo do arquivo como um vetor de tarefas.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Retrocede o arquivo após lê-lo.
+    file.seek(SeekFrom::Start(0))?;
+
+    // Grava a lista de tarefas modificada de volta no arquivo.
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
 }
