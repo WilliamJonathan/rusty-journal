04479ba40e0bc44573510fbcd7ab835b5938b114
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2024-09-11 18:00:38 -0300

    programa concluido

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::anyhow;
+use std::path::PathBuf;
 use structopt::StructOpt;
 mod cli;
 mod tasks;
@@ -5,7 +7,13 @@ mod tasks;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
 
-fn main() {
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+fn main() -> anyhow::Result<()>{
     // Obtenha os argumentos da linha de comando.
     let CommandLineArgs {
         action,
@@ -13,13 +21,15 @@ fn main() {
     } = CommandLineArgs::from_args();
 
     // Descompacte o arquivo do diário.
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     // Execute a ação.
     match action {
         Add { task } => tasks::add_task(journal_file, Task::new(task)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+    Ok(())
 }
