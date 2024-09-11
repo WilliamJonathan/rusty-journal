591ba994588e9d8f27a9036b2d15abc97dcdaec3
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2024-09-11 17:24:24 -0300

    primeiro teste, add task, list e done task ( aula 07 )

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-mod cli;
 use structopt::StructOpt;
+mod cli;
+mod tasks;
 
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
 
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // Obtenha os argumentos da linha de comando.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Descompacte o arquivo do diário.
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    // Execute a ação.
+    match action {
+        Add { task } => tasks::add_task(journal_file, Task::new(task)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -3,8 +3,8 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
-use std::io::{Result, Seek, SeekFrom};
 use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};  // Inclui o tipo `Error`.
+use std::fmt;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -43,6 +43,26 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     Ok(())
 }
 
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    // Abra o arquivo.
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    // Analise o arquivo e colete as tarefas.
+    let tasks = collect_tasks(&file)?;
+
+    // Enumerar e exibir tarefas, se houver.
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
+    Ok(())
+}
+
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
     // Abra o arquivo.
     let file = OpenOptions::new()
@@ -63,4 +83,11 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
     file.set_len(0)?;
     serde_json::to_writer(file, &tasks)?;
     Ok(())
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
 }
