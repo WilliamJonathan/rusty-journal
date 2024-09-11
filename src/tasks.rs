7b2a2dd00cf58de72856b91ff7811b21b29214da
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2024-09-11 15:29:32 -0300

    refatorando codigo ( aula 06 )

--- src/tasks.rs
+++ src/tasks.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
 use std::io::{Result, Seek, SeekFrom};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};  // Inclui o tipo `Error`.
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -19,27 +20,47 @@ impl Task {
     }
 }
 
-pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    //Abra o arquivo.
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(journal_path)?;
-
-    // Consome o conteúdo do arquivo como um vetor de tarefas.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?; // Retrocede o arquivo antes.
+    let tasks = match serde_json::from_reader(file) {
         Ok(tasks) => tasks,
         Err(e) if e.is_eof() => Vec::new(),
         Err(e) => Err(e)?,
     };
+    file.seek(SeekFrom::Start(0))?; // Retrocede o arquivo depois.
+    Ok(tasks)
+}
 
-    // Retrocede o arquivo após lê-lo.
-    file.seek(SeekFrom::Start(0))?;
-
-    // Grava a lista de tarefas modificada de volta no arquivo.
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+    let mut tasks = collect_tasks(&file)?;
     tasks.push(task);
     serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
 
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    // Abra o arquivo.
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    // Consome o conteúdo do arquivo como um vetor de tarefas.
+    let mut tasks = collect_tasks(&file)?;
+
+    // Tente remover a tarefa.
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+    tasks.remove(task_position - 1);
+
+    // Grava a lista de tarefas modificada de volta no arquivo.
+    file.set_len(0)?;
+    serde_json::to_writer(file, &tasks)?;
     Ok(())
 }
